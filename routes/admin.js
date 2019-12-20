523caa54433c4c52abd787811124593e0c54315a
Tamás <codesigner.hu@gmail.com>
2019-12-20 22:10:09 +0100

    adding path & path helper

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../helper/path");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
